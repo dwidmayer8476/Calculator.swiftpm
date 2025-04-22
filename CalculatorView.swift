11010690c5d320b0aefad3d57920e4c211dcf5ea
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-04-22 12:26:43 -0500

    edited spacing on calculator page

--- CalculatorView.swift
+++ CalculatorView.swift
@@ -10,217 +10,210 @@ struct CalculatorView: View {
     var body: some View {
         VStack {
             NavigationStack {
+
+                Text(answer)
+                    .frame(width: 400, height: 50)
+                    .background(.gray)
+                    .clipShape(Rectangle())
                 
-                    
-                    
-                    
-                    Text(answer)
-                        .frame(width: 400, height: 50)
-                        .background(.gray)
-                        .clipShape(Rectangle())
-                    
-                    
-                    HStack {
-                        Button {
-                            currentNumber += "1"
-                            answer = currentNumber
-                        } label: {
-                            Text("1")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            currentNumber += "2"
-                            answer = currentNumber
-                        } label: {
-                            Text("2")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            currentNumber += "3"
-                            answer = currentNumber
-                        } label: {
-                            Text("3")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        
+                
+                HStack {
+                    Button {
+                        currentNumber += "1"
+                        answer = currentNumber
+                    } label: {
+                        Text("1")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
                     }
-                    HStack {
-                        Button {
-                            currentNumber += "4"
-                            answer = currentNumber
-                        } label: {
-                            Text("4")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            currentNumber += "5"
-                            answer = currentNumber
-                        } label: {
-                            Text("5")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            currentNumber += "6"
-                            answer = currentNumber
-                        } label: {
-                            Text("6")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
+                    Button {
+                        currentNumber += "2"
+                        answer = currentNumber
+                    } label: {
+                        Text("2")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
                     }
-                    HStack {
-                        Button {
-                            currentNumber += "7"
-                            answer = currentNumber
-                        } label: {
-                            Text("7")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            currentNumber += "8"
-                            answer = currentNumber
-                        } label: {
-                            Text("8")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            currentNumber += "9"
-                            answer = currentNumber
-                        } label: {
-                            Text("9")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.black)
-                                .clipShape(.circle)
-                        }
+                    Button {
+                        currentNumber += "3"
+                        answer = currentNumber
+                    } label: {
+                        Text("3")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
                     }
-                    HStack{
-                       
-                        Button {
-                            previousNumber = currentNumber
-                            currentNumber = ""
-                            operation = "+"
-                        } label: {
-                            Text("+")
-                                .foregroundStyle(.black)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.blue)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            previousNumber = currentNumber
-                            currentNumber = ""
-                            operation = "-"
-                        } label: {
-                            Text("-")
-                                .foregroundStyle(.black)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.blue)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            previousNumber = currentNumber
-                            currentNumber = ""
-                            operation = "*"
-                        } label: {
-                            Text("x")
-                                .foregroundStyle(.black)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.blue)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            previousNumber = currentNumber
-                            currentNumber = ""
-                            operation = "/"
-                        } label: {
-                            Text("/")
-                                .foregroundStyle(.black)
-                                .font(.largeTitle)
-                                .frame(width: 60, height: 60)
-                                .background(.blue)
-                                .clipShape(.circle)
-                        }
+                    
+                }
+                HStack {
+                    Button {
+                        currentNumber += "4"
+                        answer = currentNumber
+                    } label: {
+                        Text("4")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
                     }
-                    HStack {
-                        Button {
-                            if let previousnumber =
-                                Double(previousNumber), let currentnumber = Double(currentNumber) {
-                                if operation == "+" {
-                                    answer = String(previousnumber + currentnumber)
-                                }else if operation == "-" {
-                                    answer = String(previousnumber - currentnumber)
-                                }else if operation == "/" {
-                                    answer = String(previousnumber / currentnumber)
-                                }else if operation == "*" {
-                                    answer = String(previousnumber * currentnumber)
-                                }
+                    Button {
+                        currentNumber += "5"
+                        answer = currentNumber
+                    } label: {
+                        Text("5")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
+                    }
+                    Button {
+                        currentNumber += "6"
+                        answer = currentNumber
+                    } label: {
+                        Text("6")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
+                    }
+                }
+                HStack {
+                    Button {
+                        currentNumber += "7"
+                        answer = currentNumber
+                    } label: {
+                        Text("7")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
+                    }
+                    Button {
+                        currentNumber += "8"
+                        answer = currentNumber
+                    } label: {
+                        Text("8")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
+                    }
+                    Button {
+                        currentNumber += "9"
+                        answer = currentNumber
+                    } label: {
+                        Text("9")
+                            .foregroundStyle(.blue)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.black)
+                            .clipShape(.circle)
+                    }
+                }
+                HStack{
+                    
+                    Button {
+                        previousNumber = currentNumber
+                        currentNumber = ""
+                        operation = "+"
+                    } label: {
+                        Text("+")
+                            .foregroundStyle(.black)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.blue)
+                            .clipShape(.circle)
+                    }
+                    Button {
+                        previousNumber = currentNumber
+                        currentNumber = ""
+                        operation = "-"
+                    } label: {
+                        Text("-")
+                            .foregroundStyle(.black)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.blue)
+                            .clipShape(.circle)
+                    }
+                    Button {
+                        previousNumber = currentNumber
+                        currentNumber = ""
+                        operation = "*"
+                    } label: {
+                        Text("x")
+                            .foregroundStyle(.black)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.blue)
+                            .clipShape(.circle)
+                    }
+                    Button {
+                        previousNumber = currentNumber
+                        currentNumber = ""
+                        operation = "/"
+                    } label: {
+                        Text("/")
+                            .foregroundStyle(.black)
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .background(.blue)
+                            .clipShape(.circle)
+                    }
+                }
+                HStack {
+                    Button {
+                        if let previousnumber =
+                            Double(previousNumber), let currentnumber = Double(currentNumber) {
+                            if operation == "+" {
+                                answer = String(previousnumber + currentnumber)
+                            }else if operation == "-" {
+                                answer = String(previousnumber - currentnumber)
+                            }else if operation == "/" {
+                                answer = String(previousnumber / currentnumber)
+                            }else if operation == "*" {
+                                answer = String(previousnumber * currentnumber)
                             }
                         }
-                        label: {
-                            Text("Calculate")
-                                .font(.largeTitle)
-                                .foregroundStyle(.black)
-                                .background(.blue)
-                                .clipShape(RoundedRectangle(cornerRadius: 40))
-                        }
-                        
-                        
-                        
-                        Button {
-                            currentNumber = ""
-                            previousanswer = ""
-                            operation = ""
-                            answer = ""
-                        } label: {
-                            Text("RESET")
-                                .foregroundStyle(.red)
-                        }
-                        
-                        
+                    }
+                    label: {
+                        Text("Calculate")
+                            .font(.largeTitle)
+                            .foregroundStyle(.black)
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 40))
+                    }
+                    
+                    Button {
+                        currentNumber = ""
+                        previousanswer = ""
+                        operation = ""
+                        answer = ""
+                    } label: {
+                        Text("RESET")
+                            .foregroundStyle(.red)
                     }
                 }
             }
+        }
         NavigationStack{
             NavigationLink("Do trigonometry ->") {
                 TrigonometryView()
             }
         }
-        }
     }
+}
 
