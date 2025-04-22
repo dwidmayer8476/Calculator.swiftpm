4295f2080335bc540910f573ad782a0d2ea45bf6
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-04-22 12:28:43 -0500

    fixed text in button in information page

--- File.swift
+++ File.swift
@@ -4,7 +4,7 @@ struct explanationsView: View {
    
     var body: some View {
         NavigationStack {
-            NavigationLink("Learn to add, subtract, and multiply") {
+            NavigationLink("Learn to add, subtract, multiply, and divide") {
                 addSubDivMultView()
             }
             .font(.largeTitle)
