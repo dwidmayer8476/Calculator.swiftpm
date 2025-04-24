60b16c1c9ef9e74e750d2365502813e0ff4da12d
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-04-24 12:59:06 -0500

    created buttons sin cos tan

--- TrigonometryViewCalulator.swift
+++ TrigonometryViewCalulator.swift
@@ -1,15 +1,59 @@
 import SwiftUI
 
 struct TrigonometryView: View {
-    @State var ALeg = 0
-    @State var OLeg = 0
-    @State var hypotnuse = 0
-    @State var angle = 0
+    @State var ALeg:Int = 0
+    @State var OLeg:Int = 0
+    @State var hypotnuse:Int = 0
+    @State var angle:Int = 0
     var body: some View {
-        TextField("Enter value of Adjacent Leg", value: $ALeg, format: .number)
-        TextField("Enter value of Opposite Leg", value: $OLeg, format: .number)
-        TextField("Enter value of Hypotnuse", value: $hypotnuse, format: .number)
-        TextField("Enter value of Angle", value: $angle, format: .number)
+        Text("Adjacent       Opposite         Hypotnuse        Angle        ")
+        HStack {
+            TextField("Adjacent Leg", value: $ALeg, format: .number)
+                .textFieldStyle(.roundedBorder)
+            TextField("Opposite Leg", value: $OLeg, format: .number)
+                .textFieldStyle(.roundedBorder)
+            TextField("Hypotnuse", value: $hypotnuse, format: .number)
+                .textFieldStyle(.roundedBorder)
+            TextField("Angle", value: $angle, format: .number)
+                .textFieldStyle(.roundedBorder)
+        }
+   
+        Text("Enter x If value is unknown or uneeded")
+        HStack{
+            Button {
+//       start         If {
+//
+//                }
+            } label: {
+                Text("SIN")
+                    .foregroundStyle(.green)
+                    .font(.largeTitle)
+                    .frame(width: 70, height: 60)
+                    .background(.blue)
+                    .clipShape(.ellipse)
+            }
+            Button {
+                
+            } label: {
+                Text("COS")
+                    .foregroundStyle(.green)
+                    .font(.largeTitle)
+                    .frame(width: 70, height: 60)
+                    .background(.blue)
+                    .clipShape(.ellipse)
+            }
+            Button {
+                
+            } label: {
+                Text("TAN")
+                    .foregroundStyle(.green)
+                    .font(.largeTitle)
+                    .frame(width: 70, height: 60)
+                    .background(.blue)
+                    .clipShape(.ellipse)
+            }
+            
+        }
     }
 }
 
