d7966736aa0977a8d38f09327df395049e4d03c8
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-04-24 13:08:12 -0500

    Fixed Alignment

--- TrigonometryViewCalulator.swift
+++ TrigonometryViewCalulator.swift
@@ -4,26 +4,36 @@ struct TrigonometryView: View {
     @State var ALeg:Int = 0
     @State var OLeg:Int = 0
     @State var hypotnuse:Int = 0
-    @State var angle:Int = 0
+    @State var angle = ""
+    //    @State var angleInput = ""
+    @State var useDegrees: Bool = true
+    var angleInRadians: Double {
+        guard let angle = Double(angle) else {return 0 }
+        return useDegrees ? angle * .pi / 180 : angle
+    }
     var body: some View {
         Text("Adjacent       Opposite         Hypotnuse        Angle        ")
         HStack {
             TextField("Adjacent Leg", value: $ALeg, format: .number)
                 .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
             TextField("Opposite Leg", value: $OLeg, format: .number)
                 .textFieldStyle(.roundedBorder)
+            keyboardType(.decimalPad)
             TextField("Hypotnuse", value: $hypotnuse, format: .number)
                 .textFieldStyle(.roundedBorder)
-            TextField("Angle", value: $angle, format: .number)
+            keyboardType(.decimalPad)
+            TextField("Enter Angle", text: $angle)
                 .textFieldStyle(.roundedBorder)
+            keyboardType(.decimalPad)
         }
-   
+        
         Text("Enter x If value is unknown or uneeded")
         HStack{
             Button {
-//       start         If {
-//
-//                }
+                //       start         If {
+                //
+                //                }
             } label: {
                 Text("SIN")
                     .foregroundStyle(.green)
