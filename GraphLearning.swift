618c8d5ac919806bfeceb9d54d1b1cb5a2402bb8
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-04-24 12:48:08 -0500

    made changes and started 2nd part of calculator

--- GraphLearning.swift
+++ GraphLearning.swift
@@ -4,9 +4,13 @@ struct GraphLearningView: View {
    
     var body: some View {
         NavigationStack {
+            Text("What is Graphing?")
+                .font(.largeTitle)
+            Text("Graphing is plotting lines on a coordinate plane that represent mathematical equations. An example of this is just a straight line travelling horizontaly. This could be represented by the equation  y = 0x + 2. This means that the line has a slop of zero meaning it doessn't go up (slope formula is rise / run = slope) and it has a y intercept of 2 meaning it travels through the y axis at the coordinate (0,2). There are also many other equations you can even use it to plot circles.")
             NavigationLink("Try It Out"){
                 graphingEquationsView()
             }
+            
             
         }
     }
--- TrigonometryViewCalulator.swift
+++ TrigonometryViewCalulator.swift
@@ -1,9 +1,18 @@
 import SwiftUI
 
 struct TrigonometryView: View {
-   
+    @State var ALeg = 0
+    @State var OLeg = 0
+    @State var hypotnuse = 0
+    @State var angle = 0
     var body: some View {
-        Text("functions to be added later")
+        TextField("Enter value of Adjacent Leg", value: $ALeg, format: .number)
+        TextField("Enter value of Opposite Leg", value: $OLeg, format: .number)
+        TextField("Enter value of Hypotnuse", value: $hypotnuse, format: .number)
+        TextField("Enter value of Angle", value: $angle, format: .number)
     }
 }
 
+#Preview {
+    TrigonometryView()
+}
