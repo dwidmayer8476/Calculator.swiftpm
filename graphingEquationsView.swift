805c222d95f3ccc0f6a411145b29de4713a24a37
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-05-12 12:42:57 -0500

    added webview of desmos

--- graphingEquationsView.swift
+++ graphingEquationsView.swift
@@ -1,43 +1,73 @@
 
 import SwiftUI
+import SafariServices
+import WebKit
+
 struct graphingEquationsView: View {
     @State var graph = ""
     @State var equation = ""
+    let desmosURL = URL(string: "https://www.desmos.com/calculator")!
     var body: some View {
         VStack{
-            Text("Graphing Calculator")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
             
-            VStack {
-                Text("y =")
-                TextField("e.g. x+2", text: $equation)
-                    .textFieldStyle(.roundedBorder)
-                    .padding(.horizontal)
-                VStack{
-                    
-                    GraphView()
-                        .frame(height: 300)
-                    
-                    Spacer()
-                    
-                    NavigationStack{
-                        NavigationLink("Go to Graph") {
-                            GraphView()
-                            
-                            
-                        }
-                    }
-                    
-                }
-                .font(.largeTitle)
-                
-                
-            }
+            Link("Try out in seperate tab", destination: desmosURL)
+            
+            
+            SafariView(url: desmosURL)
+        }
+    }
+    
+    struct SafariView: UIViewControllerRepresentable {
+        let url: URL
+        
+        func makeUIViewController(context: Context) -> SFSafariViewController {
+            SFSafariViewController(url: url)
+        }
+        
+        func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+            
         }
     }
 }
+
+
+
+
+
 #Preview {
     graphingEquationsView()
 }
+
+//OLD CODE FROM GRAPH
+
+//VStack{
+//    Text("Graphing Calculator")
+//        .font(.largeTitle)
+//        .padding()
+//    Spacer()
+//    
+//    VStack {
+//        Text("y =")
+//        TextField("e.g. x+2", text: $equation)
+//            .textFieldStyle(.roundedBorder)
+//            .padding(.horizontal)
+//        VStack{
+//            
+//            GraphView()
+//                .frame(height: 300)
+//            
+//            Spacer()
+//            
+//            NavigationStack{
+//                NavigationLink("Go to Graph") {
+//                    GraphView()
+//                }
+//                
+//            }
+//        }
+//        
+//    }
+//    .font(.largeTitle)
+//    
+//    
+//}
